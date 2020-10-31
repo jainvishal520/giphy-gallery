811ab69d092c556c0c20a0b8a3ad58e40b6bd53c
jainvishal520 <jainvishal520@gmail.com>
2020-10-31 20:27:17 +0530

    preloading all images

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ import { IconContext } from "react-icons";
 function App() {
   const [images, setImages] = useState([]);
   const [galleryError, setGalleryError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const loadAllImages = async (images) => {
+    // wait for all images to load
+    const promises = await images.map((g_img) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = g_img.images["480w_still"].url;
+        img.onload = () => {
+          resolve();
+        };
+        img.onerror = () => {
+          reject();
+        };
+      });
+    });
+    await Promise.all(promises);
+    // at this point all images are loaded
+    setImages(images);
+    setLoading(false);
+  };
+
   useEffect(() => {
     fetch(
       "https://api.giphy.com/v1/gifs/trending?" +
@@ -16,13 +38,12 @@ function App() {
         })
     )
       .then((res) => {
-        console.log(res);
         if (!res.ok) {
           throw new Error("Something went wrong!");
         }
         return res.json();
       })
-      .then((giphyData) => setImages(giphyData.data))
+      .then((giphyData) => loadAllImages(giphyData.data))
       .catch((error) => {
         setGalleryError(error.message);
       });
@@ -32,7 +53,11 @@ function App() {
       <IconContext.Provider value={{ className: "react-icons" }}>
         <h2>Giphy Gallery App</h2>
         <ErrorBoundary>
-          <TiledGallery galleryError={galleryError} images={images} />
+          {loading ? (
+            "loading..."
+          ) : (
+            <TiledGallery galleryError={galleryError} images={images} />
+          )}
         </ErrorBoundary>
       </IconContext.Provider>
     </div>
