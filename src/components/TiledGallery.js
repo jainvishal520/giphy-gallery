85b26d9b2bbe266a0615ca9b73fb617cb1a90ede
jainvishal520 <jainvishal520@gmail.com>
2020-10-31 16:10:36 +0530

    Tiled gallery component

--- src/components/TiledGallery.js
+++ src/components/TiledGallery.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const TiledGallery = ({ images, galleryError }) => {
-  return <div>{galleryError ? <p>{galleryError}</p> : <p>images</p>}</div>;
+  return (
+    <div>
+      {galleryError ? (
+        <p>{galleryError}</p>
+      ) : (
+        <div className="gallery-container">
+          {images.map((image) => {
+            const url = image.images["480w_still"].url;
+            return (
+              <div className="gallery-image-wrapper" key={image.id}>
+                <img src={url} alt={image.username} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TiledGallery;
